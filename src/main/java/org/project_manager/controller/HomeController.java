82704826954850f23f82d4c13eb6fef6b5bcc32c
eunbi <eunbi@nethru.co.kr>
2018-03-23 11:24:43 +0900

    version 1.0

---
+++ src/main/java/org/project_manager/controller/HomeController.java
@@ -0,0 +1,21 @@
+package org.project_manager.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	@RequestMapping("")
+	public String home(Model model) {
+//		LOGGER.info("homeController....");
+		model.addAttribute("greeting", "hello world");
+		return "main";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Eunbi
+  Date: 2018-03-23
+  Time: 오전 10:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+
+<h2>This is welcome page....</h2>
+
+<p>${greeting}</p>
+
+</body>
+</html>
